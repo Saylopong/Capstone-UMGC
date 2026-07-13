24eb273fcf827396d11d60044c221831656ff4c6
Saylopong <139192470+Saylopong@users.noreply.github.com>
2026-07-12 20:33:59 -0400

    Add character movement
    Uses unhandled_input to change correlated move_direction vector componenet (X,Y).
    
    Also, detects whether the player is already moving in an X or Y direction. This helps preven stuttering and setting character move direction to 0 by pressing and holding W then S or A then D.

--- Capstone Project/capstone-project/Scenes/Character/Character.gd
+++ Capstone Project/capstone-project/Scenes/Character/Character.gd
@@ -1,26 +1,62 @@
 extends CharacterBody2D
 
-const MOVE_SPEED: float = 100
 
-var current_interactable: Interactable_Object = null
+
+const MOVE_SPEED: int = 12500
+var move_direction: Vector2 = Vector2(0,0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
 func _unhandled_input(event: InputEvent) -> void:
-	#check for WASD inputs and adjust move flags to reflect character direction
-	pass
+	#checks whether player is currently moving in the y direction
+	#if the player is not changes move_direction Y vector to -1
+	if event.is_action_pressed("Up") && velocity.y == 0:
+		move_direction.y = -1
+		print("UP")
+	#checks whether player is currently moving in the -y direction
+	#if so when the UP is released changes move_direction y vector to 0
+	if event.is_action_released("Up") && velocity.y < 0:
+		move_direction.y = 0
+		print("UP_r")
+	#checks whether player is currently moving in the y direction
+	#if the player is not changes move_direction Y vector to 1
+	if event.is_action_pressed("Down") && velocity.y == 0 : 
+		move_direction.y = 1
+		print("DOWN")
+	#checks whether player is currently moving in the +y direction
+	#if so when the Down is released changes move_direction Y vector to 0
+	if event.is_action_released("Down") && velocity.y > 0: 
+		move_direction.y = 0
+		print("DOWN_r")
+	#checks whether player is currently moving in the x direction
+	#if the player is not changes move_direction X vector to -1
+	if event.is_action_pressed("Left") && velocity.x == 0:
+		move_direction.x = -1
+		print("LEFT")
+	#checks whether player is currently moving in the -x direction
+	#if so when the Left is released changes move_direction X vector to 0
+	if event.is_action_released("Left") && velocity.x < 0:
+		move_direction.x = 0
+		print("LEFT_r")
+	#checks whether player is currently moving in the X direction
+	#if the player is not changes move_direction X vector to 1
+	if event.is_action_pressed("Right") && velocity.x == 0:
+		move_direction.x = 1
+		print("RIGHT")
+	#checks whether player is currently moving in the +x direction
+	#if so when the Right is released changes move_direction X vector to 0
+	if event.is_action_released("Right") && velocity.x > 0:
+		move_direction.x = 0
+		print("RIGHT_r")
+
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+		pass
 
 func _physics_process(delta: float) -> void:
-	#adjust player velocity to movespeed in the direct when pressed and uses set_vector
-	pass
-
-func get_movement() -> Vector2:
-	#returns character movement vector multiplied by MOVE_SPEED
-	return Vector2(0,0)
-
-func interact() -> void:
-	#Interact with object	
-	pass
-
-func set_current_interactable(interactable: Interactable_Object) -> void:
-	#used to tell character what it is currently in the interaction zone of an Interactable_Obhect
-	#Is passed null if player leaves an interaction zone of an Interactable_Object
-	current_interactable = interactable
+		velocity = delta*move_direction*MOVE_SPEED
+		move_and_slide()
